0ea974b4c02674f61478b73b793fe516f8fb41c1
freddylimpens <freddy.limpens@gmail.com>
2015-09-15 16:10:02 +0200

    toIMS script done

--- toIMS.py
+++ toIMS.py
@@ -13,6 +13,14 @@ from pprint import pprint
 from yattag import indent
 from yattag import Doc
 
+FILETYPES = {
+    'weblink' : 'imswl_xmlv1p1',
+    'discussions' : 'imsdt_xmlv1p1',
+    'assessments' : 'imsqti_xmlv1p2/imscc_xmlv1p1/assessment',
+    'webcontent' : 'webcontent',
+}
+
+
 def usage():
     str = """
 Usage:
@@ -26,20 +34,7 @@ Usage:
 
 def replaceLink(link):
     """ Replace __BASE__ in urls with base given un config file toIMSconfig.json """
-    return link.replace("__BASE__/", data["base_url"])
-
-
-def convert(filein, fileout):
-
-    tree = html.fromstring(filein)
-    cleaner(tree)
-
-    body = tree.find('body')
-
-    body.rewrite_links(replaceLink)
-    f = open(fileout,"wb")
-    f.write(html.tostring(body))
-    f.close()
+    return link.replace("__BASE__/", '')
 
 
 def generateIMSManifest():
@@ -53,7 +48,7 @@ def generateIMSManifest():
     doc, tag, text = Doc().tagtext()
     # open tag 'manifest' with default content:
     doc.asis('<?xml version="1.0" encoding="UTF-8"?><manifest xmlns="http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1" xmlns:lomimscc="http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest" xmlns:lom="http://ltsc.ieee.org/xsd/imsccv1p1/LOM/resource" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" identifier="M_3E1AEC6D" xsi:schemaLocation="http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1 http://www.imsglobal.org/profile/cc/ccv1p1/ccv1p1_imscp_v1p2_v1p0.xsd http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest http://www.imsglobal.org/profile/cc/ccv1p1/LOM/ccv1p1_lommanifest_v1p0.xsd http://ltsc.ieee.org/xsd/imsccv1p1/LOM/resource http://www.imsglobal.org/profile/cc/ccv1p1/LOM/ccv1p1_lomresource_v1p0.xsd">')
-
+    # Print metadata
     with tag('metadata'):
         with tag('schema'):
             text('IMS Common Cartridge')
@@ -73,22 +68,67 @@ def generateIMSManifest():
                         text('category')
                     with tag('lomimscc:entry'):
                         text(data["lom_metadata"]["category"])
-
+    # Print organization
+    resources = []
     with tag('organizations'):
-        with tag('origanization', identifier="organization0", structure='rooted-hierarchy'):
+        with tag('organization', identifier="organization0", structure='rooted-hierarchy'):
             with tag('item', identifier='root'):
-                for idx, section in enumerate(data["sections"]):
-                    section_id = "sec_"+(str(idx))
+                for idA, section in enumerate(data["sections"]):
+                    section_id = "sec_"+(str(idA))
                     with tag('item', identifier=section_id):
                         with tag('title'):
-                            text(str(idx))
-                        for index, subsection in enumerate(data["sections"][idx]["subsections"]):
-                            with tag('item', identifier=("subsec_"+str(idx)+"_"+str(index)), identifierref=("doc_"+str(idx)+"_"+str(index))):
+                            text(str(idA))
+                        for idB, subsection in enumerate(data["sections"][idA]["subsections"]):
+                            href = data["sections"][idA]["subsections"][idB]["source_file"]
+                            filename = href.rsplit('/',1)[1]
+                            resources.append(filename)
+                            with tag('item', identifier=("subsec_"+str(idA)+"_"+str(idB)), identifierref=("doc_"+str(idA)+"_"+str(idB))):
                                 with tag('title'):
-                                    text(data["sections"][idx]["subsections"][index]["title"])
+                                    text(data["sections"][idA]["subsections"][idB]["title"])
+    # Print resources
+    with tag('resources'):
+        # retrieve images and add dependency when needed
+        doc.asis("<!-- Images -->")
+        images = {}
+        for idx, filename in enumerate(os.listdir(os.getcwd()+'/img')):
+            if filename in resources:
+                pass # avoid duplicating resources
+            else:
+                doc_id = "img_"+str(idx)
+                images[filename] = doc_id
+                with tag('resource', identifier=doc_id, type="webcontent", href="img/"+filename):
+                    doc.stag('file', href="img/"+filename)
 
+        doc.asis("<!-- Webcontent -->")
+        for idA, section in enumerate(data["sections"]):
+            for idB, subsection in enumerate(data["sections"][idA]["subsections"]):
+                doc_id = "doc_"+str(idA)+"_"+str(idB)
+                file_type = FILETYPES[data["sections"][idA]["subsections"][idB]["type"]]
+                href = data["sections"][idA]["subsections"][idB]["source_file"]
+                with tag('resource', identifier=doc_id, type=file_type, href=href):
+                     doc.stag('file', href=href)
+                     # add dependency if needed (html only)
+                     if file_type == "webcontent":
+                         html_doc = html.parse(href)
+                         img_sources = html_doc.xpath('//@src')
+                         for img in img_sources:
+                             img = img.rsplit('/', 1)[1]
+                             if img in images:
+                                 # add dependency
+                                 doc.stag('dependency', identifierref=images[img])
+                         # rewrite absolute href links
+                         body = html_doc.find('body')
+                         body.rewrite_links(replaceLink, base_href=data["base_url"])
+                         f = open(href,"wb")
+                         f.write(html.tostring(body))
+                         f.close()
 
-    print (indent(doc.getvalue()))
+    doc.asis("</manifest>")
+    imsfile = open('imsmanifest.xml', 'w')
+    imsfile.write(indent(doc.getvalue()))
+    imsfile.close()
+    return True
+
 
 def main(argv):
     """ toIMS is a utility to help building imscc archives for exporting curent material to Moodle"""
@@ -100,18 +140,8 @@ def main(argv):
 
     generateIMSManifest()
 
-    # with zipfile.ZipFile(dirin,'r') as myzip:
-    #     for orig in myzip.namelist():
-    #         print(orig)
-    #         dest = os.path.join(dirout,orig)
-    #         if orig.endswith('/'):
-    #             os.makedirs(dest,exist_ok=True)
-    #         elif orig.endswith(".html"):
-    #             f = myzip.read(orig)
-    #             convert(f,dest)
-    #         else :
-    #             myzip.extract(orig,dirout)
-
+    # Compress all directory
+    
 
 ############### main ################
 if __name__ == "__main__":
